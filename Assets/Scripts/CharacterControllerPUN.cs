c778c5d6fa944ffbe2dacb0c128a03ae5798b1ec
checker888 <ort030627@gmail.com>
2025-06-11 23:24:15 +0900

    charactercontroller修正
    photonview.ismine

--- Assets/Scripts/CharacterControllerPUN.cs
+++ Assets/Scripts/CharacterControllerPUN.cs
@@ -24,7 +24,7 @@ public class CharacterControllerPun : MonoBehaviourPun, IPunObservable
 
     void Update()
     {
-        if(PhotonNetwork.IsMasterClient)
+        if(photonView.IsMine)
         {
             MousePressed();
             KeyPressed();
