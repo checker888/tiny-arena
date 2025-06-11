fa0782748af5e3dc7fa048561971b9a3e45d4b88
checker888 <ort030627@gmail.com>
2025-06-12 01:58:10 +0900

    画面固定・簡易カーソル追加

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public bool cursorLock = true;
+    public Texture2D customCursor;
+    public Vector2 hotspot = Vector2.zero;
+
+    private bool cursorApplied = false;
+
+    void Update()
+    {
+        UpdateCursorLock();
+        DrawMyCursor();
+    }
+
+    public void UpdateCursorLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cursorLock = false;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            cursorLock = true;
+        }
+
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    void DrawMyCursor()
+    {
+        if (cursorLock && !cursorApplied && customCursor != null)
+        {
+            Cursor.SetCursor(customCursor, hotspot, CursorMode.Auto);
+            cursorApplied = true;
+        }
+        else if (!cursorLock && cursorApplied)
+        {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+            cursorApplied = false;
+        }
+    }
+
+}
